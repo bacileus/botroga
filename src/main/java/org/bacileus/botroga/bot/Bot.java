8d951d9b45ff8225e23cf9fedcc3b99812e1e547
bacileus <galejandrerh7@gmail.com>
2022-11-27 20:38:37 +0100

    fix possible issue on reconnect

--- src/main/java/org/bacileus/botroga/bot/Bot.java
+++ src/main/java/org/bacileus/botroga/bot/Bot.java
@@ -3,6 +3,7 @@ package org.bacileus.botroga.bot;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+
 import org.bacileus.botroga.listeners.PingCmdListener;
 import org.bacileus.botroga.listeners.PlaylistCmdListener;
 
