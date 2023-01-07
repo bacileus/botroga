3d56c0f5b7fad6bb6c2372b43213207ba264ef0a
bacileus <galejandrerh7@gmail.com>
2023-01-08 00:27:47 +0100

    updated arch. AudioPlayerSendHandler

--- src/main/java/org/bacileus/botroga/modules/playlist/AudioPlayerSendHandler.java
+++ src/main/java/org/bacileus/botroga/modules/playlist/AudioPlayerSendHandler.java
@@ -1,6 +1,5 @@
 package org.bacileus.botroga.modules.playlist;
 
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
 
 import net.dv8tion.jda.api.audio.AudioSendHandler;
@@ -9,12 +8,12 @@ import java.nio.ByteBuffer;
 
 public class AudioPlayerSendHandler implements AudioSendHandler {
     private static final int BYTE_BUFFER_CAPACITY = 1024;
-    private final AudioPlayer m_audioPlayer;
+    private final GuildMusicManager m_guildMusicManager;
     private final ByteBuffer m_byteBuffer;
     private final MutableAudioFrame m_mutableAudioFrame;
 
-    public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
-        m_audioPlayer = audioPlayer;
+    public AudioPlayerSendHandler(GuildMusicManager guildMusicManager) {
+        m_guildMusicManager = guildMusicManager;
         m_byteBuffer = ByteBuffer.allocate(BYTE_BUFFER_CAPACITY);
         m_mutableAudioFrame = new MutableAudioFrame();
 
@@ -23,7 +22,7 @@ public class AudioPlayerSendHandler implements AudioSendHandler {
 
     @Override
     public boolean canProvide() {
-        return m_audioPlayer.provide(m_mutableAudioFrame);
+        return m_guildMusicManager.getAudioPlayer().provide(m_mutableAudioFrame);
     }
 
     @Override
